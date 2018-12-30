eaa30dad3c7996c457e2ec8c8e50818aa11da2b0
b1tlite <b1tlite@hotmail.com>
2018-12-30 16:52:44 +0200

    I`m back!

--- Server.cpp
+++ Server.cpp
@@ -3,12 +3,20 @@
 #include "picojson.h"
 #include <cpr/cpr.h>
 
-string apiKey = "RGAPI-2232dce3-0542-44b7-9bbf-00ae436778dd";
+string apiKey = "RGAPI-22c37a55-b8bd-4f20-9f44-dac1b550080e";
 
 void Server::addPlayer(string playerName)
 {
     auto response = cpr::Get(cpr::Url{"https://" + m_name + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + playerName + "?api_key=" + apiKey});
     picojson::value v;
     auto json = picojson::parse(v, response.text);
-    std::cout << v.serialize() << std::endl;
+
+    string accountId = v.get("accountId").to_str();
+
+    Player player(playerName, accountId, {});
+
+    m_dns.insert(std::make_pair(playerName, accountId));
+    m_data.insert(std::make_pair(accountId, player));
+
+    std::cout << accountId << std::endl;
 }
--- Server.h
+++ Server.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Player.h"
+#include "Game.h"
 
 #include <map>
 #include <string>
